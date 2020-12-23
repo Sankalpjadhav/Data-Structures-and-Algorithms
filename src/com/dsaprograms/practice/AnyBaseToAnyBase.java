0102a64e600fa06ff01a57d35074d22a36e56ade
sankalpjadhav <sanky43jadhav@gmail.com>
2020-12-23 10:29:42 +0530

    Any base to any base conversion.

--- src/com/dsaprograms/practice/AnyBaseToAnyBase.java
+++ src/com/dsaprograms/practice/AnyBaseToAnyBase.java
@@ -11,14 +11,33 @@ public class AnyBaseToAnyBase {
     }
 
     public static int anyBaseToAnyBase(int n, int sourceBase, int destinationBase){
+        int inDecimal = getInDecimal(n, sourceBase);
+        int inDestinalBase = getInDestinationBase(inDecimal, destinationBase);
+        return inDestinalBase;
+    }
+
+    public static int getInDecimal(int n, int sourceBase){
+        int inDecimal = 0;
+        int i = 1;
+        while(n>0){
+            int remainder = n % 10;
+            inDecimal = inDecimal + remainder * i;
+            i = i * sourceBase;
+            n = n / 10;
+        }
+        return inDecimal;
+    }
+
+    public static int getInDestinationBase(int n, int destinationBase){
         int result = 0;
-        int i=1;
+        int i = 1;
         while(n>0){
             int remainder = n % destinationBase;
             result = result + remainder * i;
-            i = i * sourceBase;
+            i = i * 10;
             n = n / destinationBase;
         }
         return result;
     }
+
 }
