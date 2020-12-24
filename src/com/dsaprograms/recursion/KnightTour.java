f32213a91f28abd2384a9c03b4022648cae7e706
sankalpjadhav <sanky43jadhav@gmail.com>
2020-12-24 18:39:40 +0530

    Print knights tour.

---
+++ src/com/dsaprograms/recursion/KnightTour.java
@@ -0,0 +1,53 @@
+package com.dsaprograms.recursion;
+import java.util.Scanner;
+/*
+1. You are given a number n, the size of a chess board.
+2. You are given a row and a column, as a starting point for a knight piece.
+3. You are required to generate the all moves of a knight starting in (row, col) such that knight visits
+     all cells of the board exactly once.
+4. Complete the body of printKnightsTour function - without changing signature - to calculate and
+     print all configurations of the chess board representing the route
+     of knight through the chess board. Use sample input and output to get more idea.
+ */
+public class KnightTour {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] chess = new int[n][n];
+        printKnightsTour(chess, r, c, 1);
+    }
+
+    public static void printKnightsTour(int[][] chess, int r, int c, int move) {
+        if (r < 0 || c < 0 || r >= chess.length || c >= chess[0].length || chess[r][c] > 0) {
+            return;
+        } else if (move == chess.length * chess[0].length) {
+            chess[r][c] = move;
+            displayBoard(chess);
+            chess[r][c] = 0;
+            return;
+        }
+        chess[r][c] = move;
+        printKnightsTour(chess, r - 2, c + 1, move + 1);
+        printKnightsTour(chess, r - 1, c + 2, move + 1);
+        printKnightsTour(chess, r + 1, c + 2, move + 1);
+        printKnightsTour(chess, r + 2, c + 1, move + 1);
+        printKnightsTour(chess, r + 2, c - 1, move + 1);
+        printKnightsTour(chess, r + 1, c - 2, move + 1);
+        printKnightsTour(chess, r - 1, c - 2, move + 1);
+        printKnightsTour(chess, r - 2, c - 1, move + 1);
+        chess[r][c] = 0;
+    }
+
+    public static void displayBoard(int[][] chess) {
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[0].length; j++) {
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
