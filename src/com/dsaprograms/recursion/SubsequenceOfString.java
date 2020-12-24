911bdc9f2cce48737207e8a8d28017a5413040c2
sankalpjadhav <sanky43jadhav@gmail.com>
2020-12-24 09:41:47 +0530

    Recursion problems.

---
+++ src/com/dsaprograms/recursion/SubsequenceOfString.java
@@ -0,0 +1,33 @@
+package com.dsaprograms.recursion;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class SubsequenceOfString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        ArrayList<String> result =  getSubsequences(str);
+        System.out.println(result);
+    }
+    public static ArrayList<String> getSubsequences(String str){
+        if(str.length()==0){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+        char first = str.charAt(0);
+        String remaining = str.substring(1);
+        ArrayList<String> remainingResult = getSubsequences(remaining);
+        ArrayList<String> result = new ArrayList<String>();
+        // Not including first.
+        for(String s: remainingResult){
+            s=""+s;
+            result.add(s);
+        }
+        // Including first.
+        for(String s: remainingResult){
+            s=first+s;
+            result.add(s);
+        }
+        return result;
+    }
+}
