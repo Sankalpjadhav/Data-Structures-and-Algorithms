35d3fdf3e166bf05c8dbaa9f2bb071c9539626a6
sankalpjadhav <sanky43jadhav@gmail.com>
2020-12-25 11:03:40 +0530

    Partition an array based on pivot.

---
+++ src/com/dsaprograms/basedonsorting/PartitionArrayBasedOnPivot.java
@@ -0,0 +1,43 @@
+package com.dsaprograms.basedonsorting;
+import java.util.Scanner;
+public class PartitionArrayBasedOnPivot {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int pivot = scn.nextInt();
+        partition(arr,pivot);
+        print(arr);
+    }
+    public static void partition(int[] arr, int pivot){
+        //[12,3,7,2,9] Pivot: 5
+        int i=0;
+        int j=0;
+        while(j<arr.length){
+            if(arr[j]<=pivot){
+                swap(arr, i, j);
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+    // used for swapping ith and jth elements of array
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
