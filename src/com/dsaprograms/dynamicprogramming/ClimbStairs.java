393cc0b7471ab1489af46c88b9eda7031f7b7d3d
sankalpjadhav <sanky43jadhav@gmail.com>
2020-12-26 08:24:56 +0530

    Climb stairs count.

---
+++ src/com/dsaprograms/dynamicprogramming/ClimbStairs.java
@@ -0,0 +1,27 @@
+package com.dsaprograms.dynamicprogramming;
+import java.util.Scanner;
+public class ClimbStairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = climbStairs(n,new int[n+1]);
+        System.out.println(count);
+    }
+    public static int climbStairs(int n, int [] questionBank){
+        if(n==0){
+            return 1;
+        }
+        else if(n<0){
+            return 0;
+        }
+        if(questionBank[n]!=0){
+            return questionBank[n];
+        }
+        int forPathOne=climbStairs(n-1,questionBank);
+        int forPathTwo=climbStairs(n-2,questionBank);
+        int forPathThree=climbStairs(n-3,questionBank);
+        int count = forPathOne + forPathTwo + forPathThree;
+        questionBank[n] = count;
+        return count;
+    }
+}
