cd78a0030a9c7cb1a776a6e2a66e1c5d7bf02140
sankalpjadhav <sanky43jadhav@gmail.com>
2020-12-28 13:01:42 +0530

    01Knapsack problem.

--- src/com/dsaprograms/dynamicprogramming/Knapsack01.java
+++ src/com/dsaprograms/dynamicprogramming/Knapsack01.java
@@ -1,5 +1,16 @@
 package com.dsaprograms.dynamicprogramming;
 import java.util.Scanner;
+/*
+1. You are given a number n, representing the count of items.
+2. You are given n numbers, representing the values of n items.
+3. You are given n numbers, representing the weights of n items.
+3. You are given a number "cap", which is the capacity of a bag you've.
+4. You are required to calculate and print the maximum value that can be created in the bag without
+     overflowing it's capacity.
+
+Note -> Each item can be taken 0 or 1 number of times. You are not allowed to put the same item
+               again and again.
+ */
 public class Knapsack01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
