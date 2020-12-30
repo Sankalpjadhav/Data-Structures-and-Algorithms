19b4d492d39df1bf7a865973b4100d9ea5eaafdd
sankalpjadhav <sanky43jadhav@gmail.com>
2020-12-30 20:15:17 +0530

    Leetcode 91. Decode ways.

--- src/com/dsaprograms/dynamicprogramming/DecodeWays91.java
+++ src/com/dsaprograms/dynamicprogramming/DecodeWays91.java
@@ -22,32 +22,41 @@ public class DecodeWays91 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int [] dp = new int[str.length()];
-        dp[0]=1;
-        for(int i=1;i<str.length();i++){
-            if(str.charAt(i-1)=='0' && str.charAt(i)=='0'){
-                dp[i]=0;
+
+        if(str.length()==1){
+            if(str.charAt(0)=='0'){
+                System.out.println(0);
             }
-            else if(str.charAt(i-1)=='0' && str.charAt(i)!='0'){
-                dp[i]= dp[i-1];
-            }
-            else if(str.charAt(i-1)!='0' && str.charAt(i)=='0'){
-                if(Integer.parseInt(str.substring(i-1,i+1))<=26){
-                    dp[i]=i>=2?dp[i-2]:0;
-                }
-                else{
-                    dp[i]=0;
-                }
-            }
-            else{ //str.charAt(i-1)!='0' && str.charAt(i)!='0'
-                if(Integer.parseInt(str.substring(i-1,i+1))<=26){
-                    dp[i] = dp[i-1] + (i>=2 ?dp[i-2]:1);
-                }
-                else{
-                    dp[i] = dp[i-1];
-                }
+            else{
+                System.out.println(1);
             }
         }
-        System.out.println(dp[str.length()-1]);
+        int [] dp = new int[str.length()];
+        if(str.charAt(0)=='0') {
+            System.out.println(0);
+        }
+        else {
+            dp[0] = 1;
+            for (int i = 1; i < str.length(); i++) {
+                if (str.charAt(i - 1) == '0' && str.charAt(i) == '0') {
+                    dp[i] = 0;
+                } else if (str.charAt(i - 1) == '0' && str.charAt(i) != '0') {
+                    dp[i] = dp[i - 1];
+                } else if (str.charAt(i - 1) != '0' && str.charAt(i) == '0') {
+                    if (Integer.parseInt(str.substring(i - 1, i + 1)) <= 26) {
+                        dp[i] = i >= 2 ? dp[i - 2] : 1;
+                    } else {
+                        dp[i] = 0;
+                    }
+                } else { //s.charAt(i-1)!='0' && s.charAt(i)!='0'
+                    if (Integer.parseInt(str.substring(i - 1, i + 1)) <= 26) {
+                        dp[i] = dp[i - 1] + (i >= 2 ? dp[i - 2] : 1);
+                    } else {
+                        dp[i] = dp[i - 1];
+                    }
+                }
+            }
+            System.out.println(dp[str.length() - 1]);
+        }
     }
 }
