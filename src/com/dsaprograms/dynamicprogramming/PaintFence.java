43a40959f3c1f233173d1e206080d330b8ba75bb
sankalpjadhav <sanky43jadhav@gmail.com>
2020-12-31 18:28:01 +0530

    276.LeetCode: Paint fence.

--- src/com/dsaprograms/dynamicprogramming/PaintFence.java
+++ src/com/dsaprograms/dynamicprogramming/PaintFence.java
@@ -1,7 +1,7 @@
 package com.dsaprograms.dynamicprogramming;
 import java.util.Scanner;
 /*
-LeetCode-226.
+LeetCode-276.
 1. You are given a number n and a number k in separate lines, representing the number of fences and number of colors.
 2. You are required to calculate and print the number of ways in which the fences could be painted so that not more than two consecutive
 fences have same colors.
