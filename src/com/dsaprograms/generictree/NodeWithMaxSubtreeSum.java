609431ea5bd1434ee9d3af8a244c868faea5f5f8
sankalpjadhav <sanky43jadhav@gmail.com>
2021-01-15 12:44:21 +0530

    Node with Maximum Subtree Sum.

--- src/com/dsaprograms/generictree/NodeWithMaxSubtreeSum.java
+++ src/com/dsaprograms/generictree/NodeWithMaxSubtreeSum.java
@@ -68,7 +68,6 @@ public class NodeWithMaxSubtreeSum {
         return sum;
     }
 
-
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
