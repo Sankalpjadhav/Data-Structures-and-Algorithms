6ea5df0f96d81e47d490fbb457dfea2ecd099eb1
sankalpjadhav <sanky43jadhav@gmail.com>
2021-01-20 10:29:54 +0530

    Add node in BST.

---
+++ src/com/dsaprograms/binarysearchtree/AddNodeInBST.java
@@ -0,0 +1,136 @@
+package com.dsaprograms.binarysearchtree;
+import java.util.Scanner;
+import java.util.Stack;
+/*
+1. You are given a partially written BST class.
+2. You are required to complete the body of add function. "add" function is expected to add a new node with given data to the tree and return the new root.
+3. Input and Output is managed for you.
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+Constraints
+None
+Sample Input
+23
+50 25 12 n n 37 30 n n 40 n n 75 62 60 n n 70 n n 87 n n
+61
+Sample Output
+25 <- 50 -> 75
+12 <- 25 -> 37
+. <- 12 -> .
+30 <- 37 -> 40
+. <- 30 -> .
+. <- 40 -> .
+62 <- 75 -> 87
+60 <- 62 -> 70
+. <- 60 -> 61
+. <- 61 -> .
+. <- 70 -> .
+. <- 87 -> .
+ */
+public class AddNodeInBST {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack < > ();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
+    public static void display(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        String str = "";
+        str += node.left == null ? "." : node.left.data + "";
+        str += " <- " + node.data + " -> ";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
+
+    public static Node addNodeInBST(Node node, int data){
+        if(node==null){
+            return new Node(data, null, null);
+        }
+        if(data > node.data){
+            node.right = addNodeInBST(node.right,data);
+        }
+        else if(data < node.data){
+            node.left = addNodeInBST(node.left, data);
+        }
+        else{
+            // Do nothing
+        }
+        return node;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        Integer[] array = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
+        Node root = construct(array);
+        int data = sc.nextInt();
+        addNodeInBST(root,data);
+        display(root);
+    }
+}
+
