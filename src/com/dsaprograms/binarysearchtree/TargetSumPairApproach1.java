c15f069bb114f859a25197fc01e5049bf55b7263
sankalpjadhav <sanky43jadhav@gmail.com>
2021-01-20 12:56:12 +0530

    O(nlogn):Target sum pair approach 1.

--- src/com/dsaprograms/binarysearchtree/TargetSumPairApproach1.java
+++ src/com/dsaprograms/binarysearchtree/TargetSumPairApproach1.java
@@ -2,7 +2,25 @@ package com.dsaprograms.binarysearchtree;
 
 import java.util.Scanner;
 import java.util.Stack;
-
+/*
+Complexity - O(nlogn), logn to find and n to visit all node once.
+1. You are given a partially written BST class.
+2. You are given a value. You are required to print all pair of nodes which add up to the given value.
+Make sure all pairs print the smaller value first and avoid duplicacies. Make sure to print the pairs in increasing order.
+3. Input and Output is managed for you.
+Output Format
+"smaller node" "larger node"
+.. all pairs that add to target on separate lines
+Constraints
+None
+Sample Input
+21
+50 25 12 n n 37 30 n n n 75 62 60 n n 70 n n 87 n n
+100
+Sample Output
+25 75
+30 70
+ */
 public class TargetSumPairApproach1 {
     public static class Node {
         int data;
