f750a264567ba2bd368881757f6ea198e7bc3c5a
sankalpjadhav <sanky43jadhav@gmail.com>
2021-01-20 10:30:45 +0530

    Add node in BST.

--- src/com/dsaprograms/binarysearchtree/AddNodeInBST.java
+++ src/com/dsaprograms/binarysearchtree/AddNodeInBST.java
@@ -5,12 +5,6 @@ import java.util.Stack;
 1. You are given a partially written BST class.
 2. You are required to complete the body of add function. "add" function is expected to add a new node with given data to the tree and return the new root.
 3. Input and Output is managed for you.
-Input Format
-Input is managed for you
-Output Format
-Output is managed for you
-Constraints
-None
 Sample Input
 23
 50 25 12 n n 37 30 n n 40 n n 75 62 60 n n 70 n n 87 n n
