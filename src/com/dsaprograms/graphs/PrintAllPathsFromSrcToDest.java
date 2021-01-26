ea27fc3c39176c60294d66a1e1e82a1bb30f81af
sankalpjadhav <sanky43jadhav@gmail.com>
2021-01-26 10:45:31 +0530

    Print all paths from source to destination in graph.

--- src/com/dsaprograms/graphs/PrintAllPathsFromSrcToDest.java
+++ src/com/dsaprograms/graphs/PrintAllPathsFromSrcToDest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /*
 1. You are given a graph, a source vertex and a destination vertex.
 2. You are required to find and print all paths between source and destination. Print
-     them in lexicographical order.
+   them in lexicographical order.
 E.g. Check the following paths
 012546
 01256
