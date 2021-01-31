88b3c2a0abf80662327fa773bbc34f484c9ca3fe
sankalpjadhav <sanky43jadhav@gmail.com>
2021-01-31 15:07:14 +0530

    Minimum Wire Required To Connect All PC's(Minimum Spanning Tree).

--- src/com/dsaprograms/graphs/PrimsAlgorithm.java
+++ src/com/dsaprograms/graphs/PrimsAlgorithm.java
@@ -4,7 +4,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
+/*
+Minimum Wire Required To Connect All PC's
+1. You are given a graph and a source vertex. The vertices represent computers and the edges
+     represent length of LAN wire required to connect them.
+2. You are required to find the minimum length of wire required to connect all PCs over a network.
+     Print the output in terms of which all PCs need to be connected, and the length of wire between
+     them.
 
+Sample Input
+7
+8
+0 1 10
+1 2 10
+2 3 10
+0 3 40
+3 4 2
+4 5 3
+5 6 3
+4 6 8
+Sample Output
+[1-0@10]
+[2-1@10]
+[3-2@10]
+[4-3@2]
+[5-4@3]
+[6-5@3]
+ */
 public class PrimsAlgorithm {
 
     static class Edges{
