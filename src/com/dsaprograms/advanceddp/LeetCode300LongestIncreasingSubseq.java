28cdcafe07a6c364623f22fb61bb784b7b75f95f
sankalpjadhav <sanky43jadhav@gmail.com>
2021-02-21 18:33:38 +0530

    LeetCode 300: Longest increasing subsequence.

--- src/com/dsaprograms/advanceddp/LeetCode300LongestIncreasingSubseq.java
+++ src/com/dsaprograms/advanceddp/LeetCode300LongestIncreasingSubseq.java
@@ -1,7 +1,7 @@
 package com.dsaprograms.advanceddp;
 import java.util.Scanner;
 /*
-LeetCode: 300
+LeetCode: 300 Longest increasing subsequence.
 Given an integer array nums, return the length of the longest strictly increasing subsequence.
 A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements.
 For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
