91a041c29c4008cf3ad6e859bbc3693cb010b606
sankalpjadhav <sanky43jadhav@gmail.com>
2021-02-25 10:18:07 +0530

    LeetCode 940. Distinct Subsequences.

--- src/com/dsaprograms/advanceddp/LeetCode940DistinctSubsequences2.java
+++ src/com/dsaprograms/advanceddp/LeetCode940DistinctSubsequences2.java
@@ -62,7 +62,7 @@ public int distinctSubseqII(String S) {
     return dp[n-1]-1;
     }
 
-    /* why +mod is required in this line dp[i]-=(((dp[last[S[i-1]]]%mod)+mod)%mod);
+            why +mod is required in this line dp[i]-=(((dp[last[S[i-1]]]%mod)+mod)%mod);
             Now for example lets take mod = 5
             lets suppose dp[i-1] = 4 &  dp[i] = 6
             now since dp[i] is more than 5, we have to take mod. Now it will become 1
@@ -72,5 +72,4 @@ public int distinctSubseqII(String S) {
             dp[i] has become negative, so I am adding mod, because technically it changes nothing in the answer! // now dp[i] will become -3+5 =2
             And because of that dp[i] has again become positive!
             we dont want dp[i] to be negative, so remember whenever there is a substraction involved in the question and a mod is involved, adding a mod becomes mandatory for us!
-            */
  */
